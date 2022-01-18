8d30835a5d53dcd78436548ef439195f66dee33e
yllai <yllai@ucla.edu>
2022-01-18 06:35:32 +0000

    add some instructions

--- hw1/testfile.R
+++ hw1/testfile.R
@@ -7,3 +7,6 @@
 #Change in develop folder.
 
 #second test
+
+#Work on develop branch, when it's done, change to main branch
+#type "git checkout develop hw1" on terminal and it will merge files from develop to main
